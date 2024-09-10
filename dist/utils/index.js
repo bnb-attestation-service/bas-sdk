d255bbe45c4bfebd31ff8d930d4b91fcf2f88f7c
Monstea <yzhao9481@gmail.com>
2024-09-11 02:26:31 +0800

    added new generated files

---
+++ dist/utils/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ZERO_BYTES32 = void 0;
+exports.ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+//# sourceMappingURL=index.js.map
